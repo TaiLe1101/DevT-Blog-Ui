23e52383806225fc230d12b3fcd1aad694c18fe3
TaiLe1101 <taic21a.th1@gmail.com>
2023-05-26 11:17:52 +0700

    update: persistedReducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import GlobalStyles from '~/components/GlobalStyles/GlobalStyles';
 import 'tippy.js/dist/tippy.css';
 import 'tippy.js/animations/scale.css';
 
 import App from '~/App';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
@@ -15,9 +16,11 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <GlobalStyles>
-                <App />
-            </GlobalStyles>
+            <PersistGate loading={null} persistor={persistor}>
+                <GlobalStyles>
+                    <App />
+                </GlobalStyles>
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
--- src/pages/Admin/Post/PostControl/PostEdit.tsx
+++ src/pages/Admin/Post/PostControl/PostEdit.tsx
@@ -1,15 +1,40 @@
 import classNames from 'classnames/bind';
-import { useParams } from 'react-router-dom';
+import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
 import { Col, Row } from 'antd';
 
 import styles from './PostControl.module.scss';
-import { ParamsPostAdminType } from '~/types/postAdmin.type';
+import { useState } from 'react';
 const cx = classNames.bind(styles);
 
 function PostEdit() {
-    const params = useParams<ParamsPostAdminType>();
+    const [editorState, setEditorState] = useState(EditorState.createEmpty());
+    const handleEditorChange = (state: EditorState) => {
+        setEditorState(state);
+    };
+
+    const saveContent = () => {
+        const contentState = editorState.getCurrentContent();
+        const contentJson = convertToRaw(contentState);
+        console.log(contentJson);
+    };
+    const handleKeyCommand = (command: string, editorState: EditorState) => {
+        if (command === 'bold') {
+            setEditorState(RichUtils.toggleInlineStyle(editorState, 'BOLD'));
+            return 'handled';
+        }
+        return 'not-handled';
+    };
+
     return (
         <div className={cx('post-edit')}>
+            <div>
+                <Editor
+                    editorState={editorState}
+                    onChange={handleEditorChange}
+                    handleKeyCommand={handleKeyCommand}
+                />
+                <button onClick={saveContent}>Lưu nội dung</button>
+            </div>
             <Row gutter={[16, 16]}>
                 <Col span={24} lg={{ span: 12 }}>
                     <div className={cx('post-edit__text-editor')}>
