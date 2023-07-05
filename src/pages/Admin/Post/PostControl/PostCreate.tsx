17444f08eb4241587e7c3e2a24df11af2a0c41aa
TaiLe1101 <taic21a.th1@gmail.com>
2023-07-05 21:37:37 +0700

    update: 50% create post ui

--- src/pages/Admin/Post/PostControl/PostCreate.tsx
+++ src/pages/Admin/Post/PostControl/PostCreate.tsx
@@ -1,13 +1,126 @@
 import classNames from 'classnames/bind';
 
 import styles from './PostControl.module.scss';
+import noImage from '~/assets/no-img.jpg';
+import { useRef, useState } from 'react';
 
 const cx = classNames.bind(styles);
 
-interface PropsTypePostCreate {}
+function PostCreate() {
+    const inputFileRef = useRef<HTMLInputElement>(null);
+    const [previewImg, setPreviewImg] = useState<string | null>(null);
 
-function PostCreate({}: PropsTypePostCreate) {
-    return <div className={cx('wrapper')}>PostCreate component</div>;
+    const [fileImg, setFileImg] = useState<File>();
+    const [title, setTitle] = useState<string>('');
+    const [desc, setDesc] = useState<string>('');
+
+    const handleAddAvatar = () => {
+        inputFileRef.current?.click();
+    };
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedFile = e.target.files?.[0];
+        if (selectedFile && selectedFile.type.startsWith('image/')) {
+            const reader = new FileReader();
+            reader.onload = () => {
+                setPreviewImg(reader.result as string);
+            };
+
+            reader.readAsDataURL(selectedFile);
+
+            setFileImg(selectedFile);
+        }
+    };
+
+    return (
+        <div className={cx('post-control')}>
+            <div className={cx('post-control__top')}>
+                <div className={cx('post-control__avt')}>
+                    <input
+                        accept="image/*"
+                        ref={inputFileRef}
+                        type="file"
+                        hidden
+                        onChange={handleFileChange}
+                    />
+                    <img
+                        src={previewImg || noImage}
+                        alt="avatar"
+                        className={cx('post-control__img')}
+                    />
+
+                    <div className={cx('post-control__wrapper-btn')}>
+                        <button
+                            className={cx(
+                                'post-control__btn',
+                                'post-control__btn--edit'
+                            )}
+                            onClick={handleAddAvatar}
+                        >
+                            <i
+                                className={cx(
+                                    'uil',
+                                    'uil-edit',
+                                    'post-control__icon'
+                                )}
+                            ></i>
+                        </button>
+                        <button
+                            className={cx(
+                                'post-control__btn',
+                                'post-control__btn--delete'
+                            )}
+                            onClick={() => setPreviewImg(null)}
+                        >
+                            <i
+                                className={cx(
+                                    'uil',
+                                    'uil-times-circle',
+                                    'post-control__icon'
+                                )}
+                            ></i>
+                        </button>
+                    </div>
+                </div>
+
+                <div className={cx('post-control__info')}>
+                    <div className={cx('post-control__wrapper-control')}>
+                        <label
+                            htmlFor="#"
+                            className={cx('post-control__label')}
+                        >
+                            Tiêu đề
+                        </label>
+                        <input
+                            type="text"
+                            className={cx('post-control__input')}
+                            placeholder="Nhập tiêu đề... "
+                            onChange={(e) => setTitle(e.target.value)}
+                        />
+                    </div>
+
+                    <div className={cx('post-control__wrapper-control')}>
+                        <label
+                            htmlFor="#"
+                            className={cx('post-control__label')}
+                        >
+                            Tóm tắt
+                        </label>
+                        <textarea
+                            cols={30}
+                            rows={8}
+                            className={cx(
+                                'post-control__input',
+                                'post-control__input--desc'
+                            )}
+                            placeholder="Nhập tóm tắt bài viết..."
+                            onChange={(e) => setDesc(e.target.value)}
+                        ></textarea>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default PostCreate;
